3191544a9017b3537bb6ba0de23da9e1d14a67e4
L015lanz <161093207+L015lanz@users.noreply.github.com>
2024-02-24 18:31:52 -0600

    Delete tarea1 directory

--- tarea1/patrones/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.cargarArchivo, name='cargarArchivo'),
-    path('resultados/<str:nombre>',views.mostrarResultados, name='resultados'),
-    path('lista-archivos',views.lista_archivos, name='listaArchivos'),
-
-]
